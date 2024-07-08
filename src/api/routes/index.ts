103e644438f4e78905c4d9955403d8c605c35ba7
Kaue Hiroyuki Hirose <kaue.hirose.pb@compasso.com.br>
2024-07-08 02:48:55 -0300

    commitando sessions 3

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import movieRouter from './movieRoutes';
+import sessionRoutes from './sessionRoutes'
+//import movieRouter from './movieRoutes';
 
 const routes = Router();
 
-routes.use('/api/v1/', movieRouter);
+//routes.use('/api/v1/', movieRouter);
+routes.use('/api/v1/', sessionRoutes)
 
 export default routes;
